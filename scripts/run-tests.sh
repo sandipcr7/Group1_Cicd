0b2c621fe701e88e5ad9e1777c6fb640dc54e1ef
sandip <sandipbharati07@gmail.com>
2026-03-17 15:33:17 -0400

    image test

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -27,4 +27,10 @@ if ! grep -q "March 17, 2026" myapp/index.html; then
   exit 1
 fi
 
+#  Check an image is present
+if ! grep -q "<img" myapp/index.html; then
+  echo "ERROR: No image found in index.html." >&2
+  exit 1
+fi
+
 echo "All required files are present and date check passed."
